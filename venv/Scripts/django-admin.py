b2158aa9dd4202cbc0163d6da710666a45620759
Ivette <car16020@uvg.edu.gt>
2018-10-01 18:14:35 -0600

    venv

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dell\docume~1\uvg\viseme~1\web\instag~1\instag~1\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
